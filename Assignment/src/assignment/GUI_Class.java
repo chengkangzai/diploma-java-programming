702ebb89ef4d375eded36b3861486926fbb7061d
chengkangzai <pycck@hotmail.com>
2020-03-10 13:27:56 +0800

    add top up file

--- Assignment/src/assignment/GUI_Class.java
+++ Assignment/src/assignment/GUI_Class.java
@@ -1,58 +1,33 @@
 package assignment;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class GUI_Class {
 
-    public static void showPopUp() {
-        //Initializing JFRAME and button
-        final JFrame parent = new JFrame();
-        JButton button = new JButton();
-        //set the button text
-        button.setText("Click me to show dialog!");
-        // add the button to the jframe
-        parent.add(button);
-        //Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
-        parent.pack();
-        //Show it
-        parent.setVisible(true);
-
-        //Event listener of Java
-        button.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                //show the dialog 
-                String name = JOptionPane.showInputDialog(parent,
-                        "What is your name?", null);
-            }
-        });
-    }
-
-    public static void test() {
+    public static void showConfirmDialog(String s) {
         final JFrame parent = new JFrame();
         parent.setVisible(true);
+
         //JOptionPane.showInputDialog(parent, "What is your name?", null);
-        //JOptionPane.showMessageDialog(parent, "A button is been click ");
-        Object[] possibilities = {"ham", "spam", "yam"};
-        String s = (String) JOptionPane.showInputDialog(
-                parent,
-                "A button is been click!",
-                "Confirmation Dialog ",
-                JOptionPane.PLAIN_MESSAGE,
-                null,
-                possibilities,
-                "ham");
-        if ("ham".equals(s)) {
-            parent.setVisible(false);
-        }else if ("spam".equals(s)){
-            parent.setVisible(false);
-        }else if ("yam".equals(s)){
-            parent.setVisible(false);
+        final int showConfirmDialog = JOptionPane.showConfirmDialog(parent, s);
+        switch (showConfirmDialog) {
+            case 0:
+                JOptionPane.showMessageDialog(parent, "Yes is return");
+                break;
+
+            case 1:
+                JOptionPane.showMessageDialog(parent, "No is return");
+                break;
+
+            case 2:
+                JOptionPane.showMessageDialog(parent, "Cacel is return");
+                break;
+
         }
+        parent.setVisible(false);
     }
     
 
-
+    
 }
