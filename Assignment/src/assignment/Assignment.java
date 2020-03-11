862b41f173dc68112928edba359c92bbf05c41d2
chengkangzai <pycck@hotmail.com>
2020-03-11 23:48:19 +0800

    testing

--- Assignment/src/assignment/Assignment.java
+++ Assignment/src/assignment/Assignment.java
@@ -5,22 +5,31 @@
  */
 package assignment;
 
-/**
- *
- * @author ASUS
- */
+import java.util.ArrayList;
+
 public class Assignment {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //DB.echoSplited();
+        //String[] ss = DB.returnAll();
+        //for (int i = 1; i < ss.length; i++) {
+        //    String ii = String.valueOf(i);
+        //    System.out.println(DB.getStockNameByID(ii));
+        //    System.out.println(DB.getStockPriceByID(ii));
+        //    System.out.println(DB.getStockByID(ii));
+        //    System.out.println(DB.getStockAvailabilityByID(ii));
+        //    System.out.println(DB.getStockImageByID(ii));
+        //}
+        //DB.addLine();
 
+        ArrayList<String> ss = DB.returnAllStockPrice();
+        for (int i = 0; i < ss.size(); i++) {
+            System.out.println(i + " is = " + ss.get(i));
+        }
         GUI gui = new GUI();
         gui.setVisible(true);
-        // TODO code application logic here
-        //TODO
         //1. Define UI Class --> GUI
         //2. Calculation Logic with proper method --> Stock 
         //3. Text Editing(db) w method --> DB
